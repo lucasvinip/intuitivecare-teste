da4bb6ebea09ec22c1dbcb6031e2dbcfe5703a05
Lucas Vinicius <l.vinicius1577@gmail.com>
2025-04-02 11:21:10 -0300

    Ajustes no projeto web-scraping e adicionando transformacao-dados

--- 1-web-scraping/scraping.py
+++ 1-web-scraping/scraping.py
@@ -3,27 +3,33 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import zipfile
-import shutil
 
-URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+URL = 'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
 }
 ANEXOS_DIR = 'anexos'
-ZIP_FILE = os.path.join(ANEXOS_DIR,'anexos_compactados.zip')
+ZIP_FILE = os.path.join(ANEXOS_DIR, 'anexos_compactados.zip')
 NECESSARY_FILES = ['Anexo_I', 'Anexo_II']
 
 def get_pdf_urls():
-    response = requests.get(URL)
+    response = requests.get(URL, headers=HEADERS)
     if response.status_code != 200:
         print('Falha ao acessar o site')
         return []
     
     soup = BeautifulSoup(response.text, 'html.parser')
-    pdf_links = soup.find_all('a', href = True)
-    pdf_urls = [link['href'] for link in pdf_links if link['href'].endswith('.pdf')]
-
-    return [url for url in pdf_urls if any(file in url for file in NECESSARY_FILES)]
+    pdf_links = soup.find_all('a', href=True)
+    pdf_urls = []
+    
+    for link in pdf_links:
+        href = link['href']
+        if href.endswith('.pdf') and any(file in href for file in NECESSARY_FILES):
+            if href.startswith('/'):
+                href = 'https://www.gov.br' + href
+            pdf_urls.append(href)
+    
+    return pdf_urls
 
 def download_pdfs():
     pdf_urls = get_pdf_urls()
@@ -31,52 +37,36 @@ def download_pdfs():
     if not pdf_urls:
         print('Nenhum PDF encontrado para download.')
         return None
-
+    
     if not os.path.exists(ANEXOS_DIR):
         os.makedirs(ANEXOS_DIR)
     
-    download_files = []
+    zip_temp = zipfile.ZipFile(ZIP_FILE, 'w', zipfile.ZIP_DEFLATED)
+    
     for pdf_url in pdf_urls:
-        pdf_name = os.path.join(ANEXOS_DIR, pdf_url.split('/')[-1])
-
-        if os.path.exists(pdf_name):
-            print(f'Já existe este arquivo: {pdf_name}')
-            continue
-
+        pdf_name = pdf_url.split('/')[-1]
         try:
             pdf_response = requests.get(pdf_url, headers=HEADERS, timeout=10)
             pdf_response.raise_for_status()
-
-            with open(pdf_name, 'wb') as f:
-                f.write(pdf_response.content)
             
-            download_files.append(pdf_name)
-            print(f'Download concluído: {pdf_name}')
+            zip_temp.writestr(pdf_name, pdf_response.content)
+            print(f'Download e compactação concluídos: {pdf_name}')
         except requests.RequestException as e:
             print(f'Erro ao baixar {pdf_url}: {e}')
         
         time.sleep(1)
     
-    if os.path.exists(pdf_name):
-        return 'anexos'
-    else:
-        None
+    zip_temp.close()
+    return ZIP_FILE
 
-def turn_zip_file():
-    downloaded_files = download_pdfs()
-
-    if not downloaded_files:
-        print('Nenhum arquivo baixado, compactação não realizada.')
-        return
+def main():
     if os.path.exists(ZIP_FILE):
-        print('Os arquivos já estão compactados')
+        print('Os arquivos já estão compactados.')
     else:
-        with zipfile.ZipFile(ZIP_FILE, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            for root, _, files in os.walk(ANEXOS_DIR):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    if file_path != ZIP_FILE:
-                        zipf.write(file_path, os.path.relpath(file_path, ANEXOS_DIR))           
-        print(f'Arquivos compactados em {ZIP_FILE}')
+        zip_path = download_pdfs()
+        if zip_path:
+            print(f'Arquivos compactados em {zip_path}')
+        else:
+            print('Nenhum arquivo foi baixado e compactado.')
 
-turn_zip_file()
+main()
---
+++ 2-transformacao-dados/transformacao_dados.py
@@ -0,0 +1,90 @@
+import os
+import pandas as pd
+import zipfile
+import pdfplumber
+
+ANEXOS_DIR = 'anexos'
+CSV_FILENAME = 'tabela_anexo_I_extraida.csv'
+ZIP_FILENAME = os.path.join(ANEXOS_DIR, 'Teste_Lucas_Vinicius.zip')
+ZIP_ANEXO = os.path.join(ANEXOS_DIR, 'anexos_compactados.zip')
+
+ABREVIACOES = {
+    "OD": "Seg. Odontológica",
+    "AMB": "Seg. Ambulatorial"
+}
+
+if os.path.exists(ZIP_FILENAME):
+    print('A tabela já existe')
+    exit()
+
+def extract_pdf_from_zip():
+    if not os.path.exists(ZIP_ANEXO):
+        print('Primeiro executar o projeto scraping.py')
+        return None
+    
+    with zipfile.ZipFile(ZIP_ANEXO, 'r') as zip_ref:
+        for file in zip_ref.namelist():
+            if 'Anexo_I' in file and file.endswith('.pdf'):
+                extracted_path = os.path.join(ANEXOS_DIR, file)
+                zip_ref.extract(file, ANEXOS_DIR)
+                print(f'PDF extraído: {extracted_path}')
+                return extracted_path
+    
+    print('Anexo I não encontrado no ZIP.')
+    return None
+
+def extract_table_from_pdf(pdf_file):
+    data = []
+    
+    with pdfplumber.open(pdf_file) as pdf:
+        for page in pdf.pages:
+            table = page.extract_table()
+            
+            if table:
+                for row in table:
+                    if any(row):
+                        data.append(row)
+    
+    return data
+
+def replace_abbreviations(data_frame):
+    return data_frame.replace(ABREVIACOES)
+
+def save_to_csv(data, output_file):
+    if not data:
+        print('Nenhuma tabela foi extraída, o CSV não será salvo.')
+        return None
+    
+    data_frame = pd.DataFrame(data)
+    data_frame = replace_abbreviations(data_frame)
+    
+    data_frame.to_csv(output_file, index=False, encoding='utf-8-sig')
+    print(f'Dados salvos em: {output_file}')
+    
+    return output_file
+
+def compress_csv(csv_file, zip_file):
+    if not os.path.exists(csv_file):
+        print(f'O arquivo {csv_file} não foi encontrado')
+        return
+    
+    with zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(csv_file, os.path.basename(csv_file))
+    
+    print(f'Arquivo compactado como {zip_file}')    
+    os.remove(csv_file)
+    print(f'Arquivo {csv_file} removido após a compactação.')
+
+pdf_path = extract_pdf_from_zip()
+
+if pdf_path:
+    extracted_data = extract_table_from_pdf(pdf_path)
+    
+    if extracted_data:
+        output_csv = os.path.join(ANEXOS_DIR, CSV_FILENAME)
+        csv_file = save_to_csv(extracted_data, output_csv)
+        if csv_file:
+            compress_csv(csv_file, ZIP_FILENAME)
+            os.remove(pdf_path)
+    else:
+        print('Nenhuma tabela encontrada no PDF')
